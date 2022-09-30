0eecfbf5bfa4b23500b2f8fb1179270bb77aa946
shankee1506 <pshankeerthan@gmail.com>
2022-09-30 21:04:48 +0530

    register user validation

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 require('dotenv/config');
-// mongoose.set("useNewUrlParser", true);
-// mongoose.set("useUnifiedTopology", true);
-// mongoose.set("useFindAndModify", false);
+
 
 
 app.get('/', (req, res) => {
@@ -16,7 +14,6 @@ app.use(express.json());
 
 //connect with router
 const userRouter = require('./routes/auth');
-const User = require('./model/User');
 app.use('/users', userRouter);
 
 
@@ -28,19 +25,3 @@ mongoose.connect(process.env.DB_CONNECTION, () => {
     console.log('Database succefully connected')
 });
 
-// mongoose.connect(process.env.DB_CONNECTION, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-
-//   });
-
-//   const connection = mongoose.connection;
-//   connection.once("open", () => {
-//     console.log("Mongodb connetction succesfull");
-//   });
-  
-// new User({
-//     name: 'shankee'
-// }).save(() => {
-//     console.log('created User')
-// })
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../model/User')
+const bcrypt = require('bcryptjs');
+const { registerValidation } =require('../validation')
 
 router.get('/registerCheck', (req,res) => {
     res.send("Check the register page");
@@ -8,20 +10,32 @@ router.get('/registerCheck', (req,res) => {
 
 router.post('/register', async (req, res) =>  {
     
+    //validate the user 
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message)
+        
+
+    // if user email exist not save in database
+    const existEmail = await User.findOne({ email: req.body.email })
+    if (existEmail) return res.status(400).send('User already Exist');
+
+    const salt = await bcrypt.genSalt(10); 
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    
     const user = new User({
         name: req.body.name, 
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try {
         const userRegister = await user.save();
-        res.json(userRegister)
+        res.status(200).send(userRegister)
     } catch (error) {
-        res.json({message:error})
+        res.status(400).send({message:error})
     }
 
-    console.log(req.body)
+
 })
 
 module.exports = router;
